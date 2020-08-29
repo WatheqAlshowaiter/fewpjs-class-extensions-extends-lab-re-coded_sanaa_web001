252542d2912be2d03c5c38f03c902d02177b651c
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-29 07:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 // Your code here
-// Your code here
-
-// 1. Define a `Polygon` class. `Polygon` will accept an `Array` of integers as a
-// parameter, which will represent each _side_ of a shape.
-// * Use the `get` keyword to make a getter method `countSides`
-//  that counts the number of sides (each index in the array).
-// * Use the `get` keyword to make a getter method `perimeter`
-// that calculates the sum of each side (each index in the array) of the polygon.
-//  This method will become accessible to its child classes.
 
 class Polygon {
   constructor(integers) {
@@ -18,5 +9,49 @@ class Polygon {
   }
   get perimeter() {
     return this.integers.reduce((acc, cur) => acc + cur, 0);
+  }
+}
+
+// 2. Define a `Triangle` class that inherits from `Polygon`.
+// * It will automatically have access to `count` and `perimeter` inherited from `Polygon`.
+// * Use the `get` keyword to make a getter method `isValid` that
+//  checks if the given 3 sides for a triangle is valid.
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (
+      this.countSides() === 3 &&
+      this.perimeter() > this.integers[0] &&
+      this.perimeter() > this.integers[1] &&
+      this.perimeter() > this.integers[2]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+// 3. Define a `Square` class that inherits from `Polygon`.
+// * It will automatically have access to `count` and `perimeter` inherited from `Polygon`.
+// * Use the `get` keyword to make a getter method `isValid` that checks if the given 4 sides
+//  for a square is
+// valid. A square is valid when the lengths of all sides are equal.
+// * Use the `get` keyword to make a getter method `area`
+//  that calculates the area of the square.
+class Square extends Polygon {
+  get isValid() {
+    if (
+      ((this.integers[0] === this.integers[1]) === this.integers[2]) ===
+      this.integers[3]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get area() {
+    return this.integers[0] ** 2;
   }
 }

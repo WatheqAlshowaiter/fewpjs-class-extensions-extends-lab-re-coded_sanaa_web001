54f42cd9147133b245500f68da01f56e638d23a4
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-29 07:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+// Your code here
+
+// 1. Define a `Polygon` class. `Polygon` will accept an `Array` of integers as a
+// parameter, which will represent each _side_ of a shape.
+// * Use the `get` keyword to make a getter method `countSides`
+//  that counts the number of sides (each index in the array).
+// * Use the `get` keyword to make a getter method `perimeter`
+// that calculates the sum of each side (each index in the array) of the polygon.
+//  This method will become accessible to its child classes.
+
+class Polygon {
+  constructor(integers) {
+    this.integers = integers;
+  }
+  get countSides() {
+    return this.integers.length;
+  }
+  get perimeter() {
+    return this.integers.reduce((acc, cur) => acc + cur, 0);
+  }
+}
